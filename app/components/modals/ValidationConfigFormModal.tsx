4b5b5a538ff9a9c00a68f437c79c08fcd32a0b0e
Adnan Karim <156935904+terabitdev@users.noreply.github.com>
2026-04-07 11:08:13 +0500

    fix: remove usage warning banner from Edit Validation Config modal

--- app/components/modals/ValidationConfigFormModal.tsx
+++ app/components/modals/ValidationConfigFormModal.tsx
@@ -158,15 +158,6 @@ export default function ValidationConfigFormModal({
           </button>
         </div>
 
-        {/* Usage warning */}
-        {initialConfig && usageCount > 0 && (
-          <div className="mx-4 mt-3 px-3 py-2 bg-amber-50 border border-amber-200 rounded-lg">
-            <p className="text-xs text-amber-700 font-medium">
-              This config is used by {usageCount} place(s). Changes will affect all of them.
-            </p>
-          </div>
-        )}
-
         {/* Form */}
         <form onSubmit={handleSubmit} className="flex-1 overflow-y-auto p-4 space-y-4">
           {formErrorMsg && (
